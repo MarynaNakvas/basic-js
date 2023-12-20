539f79f7f0b462dbb375e2cf8ee36c02441ba253
Maryna Nakvas <nakvas.maryna@gmail.com>
2023-12-20 20:48:30 +0100

    feat: resolve what season task

--- src/what-season.js
+++ src/what-season.js
@@ -12,26 +12,32 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 function getSeason(date) {
-  if (date === undefined) {
-    return 'Unable to determine the time of year!';
-  } else if (!(date instanceof Date)) {
-    throw new TypeError('Invalid date!');
-  } else {
-    let season;
-    const month = date.getMonth();
-
-    if ( month >=2 && month <= 4 ) {
-      season = 'spring';
-    } else if (month >= 5 && month <= 7) {
-      season = 'summer';
-    } else if (month >= 8 && month <= 10) {
-      season = 'fall';
-    } else if (month === 11 || month === 0 || month === 1) {
-      season = 'winter';
+  try {
+    if (date === undefined) {
+      return 'Unable to determine the time of year!';
+    } else if (!(date instanceof Date) || isNaN(date.getTime())) {
+      throw new TypeError('Invalid date!');
+    } else {
+      let season;
+      const month = date.getMonth();
+  
+      if ( month >=2 && month <= 4 ) {
+        season = 'spring';
+      } else if (month >= 5 && month <= 7) {
+        season = 'summer';
+      } else if (month >= 8 && month <= 10) {
+        season = 'fall';
+      } else if (month === 11 || month === 0 || month === 1) {
+        season = 'winter';
+      }
+   
+      return season;
     }
- 
-    return season;
-  }
+  } catch (error) {
+    if (error instanceof TypeError) {
+      throw new TypeError('Invalid date!');
+    }
+  }  
 }
 
 module.exports = {
