9d4563341391789432f5d2e4b587273cc650fa6f
Maryna Nakvas <nakvas.maryna@gmail.com>
2023-12-21 13:43:46 +0100

    feat: resolve vigenere cipher task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,56 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(value) {
+    this.class = value;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(message, key) {
+    if (message === undefined || key === undefined) {
+      throw new TypeError('Incorrect arguments!');
+    } else {
+      const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+      let encryptText = '';
+      let j = 0;
+  
+      for (let i = 0; i < message.length; i++) {
+        if (message[i].match(/[a-zA-Z]/i)) {
+          const keyValueCurrent = key[j % key.length];
+          const messageCurrentValueIndex = alphabet.indexOf(message[i].toUpperCase());
+          const moveIndex = alphabet.indexOf(keyValueCurrent.toUpperCase());
+          encryptText += alphabet[(messageCurrentValueIndex + moveIndex) % alphabet.length];
+          j++;
+        } else {
+          encryptText += message[i];
+        }
+      }
+
+      return this.class === false ? encryptText.split('').reverse().join('') : encryptText;
+    }
+  }
+
+  decrypt(message, key) {
+    if (message === undefined || key === undefined) {
+      throw new TypeError('Incorrect arguments!');
+    } else {
+      const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+      let decryptText = '';
+      let j = 0;
+  
+      for (let i = 0; i < message.length; i++) {
+        if (message[i].match(/[a-zA-Z]/i)) {
+          const keyValueCurrent = key[j % key.length];
+          const messageCurrentValueIndex = alphabet.indexOf(message[i].toUpperCase());
+          const moveIndex = alphabet.indexOf(keyValueCurrent.toUpperCase());
+          decryptText += alphabet[((messageCurrentValueIndex - moveIndex) + alphabet.length) % alphabet.length];
+          j++;
+        } else {
+          decryptText += message[i];
+        }
+      }
+
+      return this.class === false ? decryptText.split('').reverse().join('') : decryptText;
+    }
   }
 }
 
