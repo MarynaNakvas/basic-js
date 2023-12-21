048ad4fb424e5dabd6fa9d29e2439a2bd7203b5a
Maryna Nakvas <nakvas.maryna@gmail.com>
2023-12-21 18:27:24 +0100

    feat: resolve carbon dating task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,14 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  if (typeof sampleActivity !== 'string' || isNaN(Number(sampleActivity)) || Number(sampleActivity) > 15 || Number(sampleActivity) <= 0) {
+    return false;
+  } else {
+    const k = Math.log(2)/HALF_LIFE_PERIOD;
+    const t = Math.log(MODERN_ACTIVITY / sampleActivity) / k;
+    return Math.ceil(t);
+  }
 }
 
 module.exports = {
