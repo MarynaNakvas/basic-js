438e032cab1f9322d5ad45a35c50594f93551854
Maryna Nakvas <nakvas.maryna@gmail.com>
2023-12-22 10:57:18 +0100

    feat: resolve hanoi tower task

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -14,9 +14,12 @@ const { NotImplementedError } = require('../extensions/index.js');
  * calculateHanoi(9, 4308) => { turns: 511, seconds: 427 }
  *
  */
-function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function calculateHanoi(disksNumber, turnsSpeed) {
+  const transfers = 2 ** disksNumber - 1;
+  let speedPerSec = turnsSpeed / 3600;
+  let minSeconds = Math.floor(transfers / speedPerSec);
+
+  return {turns: transfers, seconds: minSeconds};
 }
 
 module.exports = {
